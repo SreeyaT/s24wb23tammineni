00c65443124f1aa9b581ef02d3fe78079c1c4fd8
SreeyaT <saisreeya1601@gmail.com>
2024-03-27 17:16:47 -0500

    assignment10

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 Hi, There!
 [Render Link](https://s24wb23tammineni.onrender.com)
 
+
+Class:
+-City
+Attributes:
+-Name (string)
+-Country (string)
+-Population (number)
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Hello Sai Sreeya' });
+  res.render('index', { title: 'Sai Sreeya' });
 });
 
 module.exports = router;
